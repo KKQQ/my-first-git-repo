9dedcd0e6f4a54fdc622b01afef324e8339d1389
KKQQ <tana_mana@foxmail.com>
2025-09-04 10:39:41 +0200

    update app.py and .gitignore

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="我的第一个 Streamlit 应用", page_icon="🧪", layout="centered")
+st.set_page_config(page_title="My first Streamlit app te", page_icon="🧪", layout="centered")
 
-st.title("Hello Streamlit 👋")
-name = st.text_input("你的名字：", placeholder="比如：Anna")
-if st.button("打招呼"):
-    st.success(f"Hi, {name or '朋友'}!")
+st.title("Hello Streamlit!")
+name = st.text_input("You name: ", placeholder="e.g.: Anna")
+if st.button("Greet me"):
+    st.success(f"Hi, {name or 'friend'}!")
 
-st.subheader("小表格")
+st.subheader("Table")
 df = pd.DataFrame({"A":[1,2,3], "B":[4,5,6]})
 st.dataframe(df)
 
-st.subheader("滑块 + 动态结果")
-x = st.slider("选择一个数字", 0, 100, 50)
-st.write("平方：", x**2)
+st.subheader("Result of a calculation")
+x = st.slider("Choose a number", 0, 100, 50)
+st.write("Square：", x**2)
